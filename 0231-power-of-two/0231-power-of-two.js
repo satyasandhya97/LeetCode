1c256f5349e56fc3160679eb6edb951cbd3e13b4
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2025-06-05 23:28:26 +0530

    Time: 0 ms (100%), Space: 57.4 MB (22.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -4,5 +4,6 @@
  */
 var isPowerOfTwo = function(n) {
     //solve it without loops/recursion?
+    //using bitwise operator n= 16(10000) & n-1=15(01111) = 00000 then its return true
     return n > 0 && (n & (n-1))===0;
 };
