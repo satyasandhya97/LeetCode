b4a05b1506428fb1194e27e96853bf002b5f15c1
Satyasandhya Biswal <satyasandhyabiswal97@gmail.com>
2025-08-05 22:21:41 +0530

    Time: 5 ms (24.4%), Space: 56.7 MB (28.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,23 +2,31 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
-    const stack = [];
-    const map = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
+var isValid = function(s) {
+  const map = []
 
-    for (let char of s) {
-        if (['(', '{', '['].includes(char)) {
-            stack.push(char);
-        } else {
-            if (stack.pop() !== map[char]) {
-                return false;
-            }
+  for(let i = 0; i < s.length; i++){
+    if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+        map.push(s[i])
+    }else {
+        let data = map.pop();
+        let compValue = '';
+        switch(s[i]){
+          case ')' :
+            compValue = '(';
+            break;
+          case '}' :
+             compValue = '{';
+             break
+          case ']' :
+             compValue = '[';
+             break
         }
-    }
+        if(data !== compValue){
+            return false;
+        }
 
-    return stack.length === 0;
+    }
+  }
+   return map.length === 0;
 };
