1ab1161863599c069b034b3206af1a8b12cc89a6
Satyasandhya Biswal <satyasandhyabiswal97@gmail.com>
2025-08-07 23:25:35 +0530

    Time: 5 ms (42.77%), Space: 55.3 MB (90.58%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let stack = [];
+    let ans = "";
+    for(let i =0;i < s.length; i++){
+        if(s[i] === '('){
+             if(stack.length > 0) ans += s[i];
+            stack.push(s[i]);
+        }else {
+            stack.pop();
+             if(stack.length > 0) ans += s[i];
+        }
+    }
+    return ans;
+};
